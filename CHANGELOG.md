27768d856539d6d9ec754af2bb4c93b2b6306180
Alex Torscho <contact@alextorscho.com>
2015-08-11 13:14:17 +0200

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+# Changelog
+
+All Notable changes to `uservel` will be documented in this file
+
+## 1.0.0
+
+### Added
+- Nothing
+
+### Deprecated
+- Nothing
+
+### Fixed
+- Nothing
+
+### Removed
+- Nothing
+
+### Security
+- Nothing
---
+++ src/UservelServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Atorscho\Uservel;
+
+use Blade;
+use Illuminate\Foundation\AliasLoader;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\ServiceProvider;
+
+class UservelServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
